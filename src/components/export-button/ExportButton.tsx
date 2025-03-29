5d2646c9d10ce115bcaf00af76028a34b62eeab4
YarsCode <yarsason@gmail.com>
2025-03-29 19:45:34 +0300

    fixed env prod path

--- src/components/export-button/ExportButton.tsx
+++ src/components/export-button/ExportButton.tsx
@@ -32,7 +32,8 @@ const ExportButton: React.FC<Props> = ({dataObj}) => {
     const exportToDocx = () => {
         return new Promise<Blob>((resolve, reject) => {
             // loadFile(`${import.meta.env.BASE_URL}planningTemplate.docx`, function (error, content) {
-            const docxUrl = import.meta.env.VITE_DOCX_PATH;
+            const docxUrl = import.meta.env.VITE_DOCX_PATH || 'planningTemplate.docx';
+            console.log("DOCX path:", import.meta.env.VITE_DOCX_PATH);
             
             loadFile(docxUrl, function (error, content) {
                 if (error) {
